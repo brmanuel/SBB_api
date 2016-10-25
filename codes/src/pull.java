2de68e44f2dd610bba12903f0a82c1f3a9713077
brmanuel <manuel.broechin@hotmail.com>
2016-10-25 20:58:36 +0200

    commit from windows

--- codes/src/pull.java
+++ codes/src/pull.java
@@ -13,10 +13,9 @@ import java.util.Scanner;
 public class pull {
 
     public static void main(String [] args) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
         ObjectMapper objectMapper = new ObjectMapper();
         URL url = new URL("http://transport.opendata.ch/v1/stationboard?station=Aarau&limit=10");
         sbb sbb1 = objectMapper.readValue(url, sbb.class);
-        System.out.println("output: " + sbb1.station);
+        //System.out.println("output: " + sbb1.station);
     }
 }
