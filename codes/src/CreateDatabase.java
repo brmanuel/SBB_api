6b5deb805127262c4dabd293968592765c3f444a
brmanuel <manuel.broechin@hotmail.com>
2016-11-14 00:04:51 +0100

    added comments

--- codes/src/CreateDatabase.java
+++ codes/src/CreateDatabase.java
@@ -2,6 +2,10 @@ import java.util.List;
 import java.util.ArrayList;
 
 //a class to create a database of (station, coordinate) which can be searched (sweep line algorithm) in order to find the closest station to location
+/*
+ * remarks for later: use google api like: http://maps.google.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA to get
+ * coordinates from address
+ */
 public class CreateDatabase {
 	
 class Entry{
